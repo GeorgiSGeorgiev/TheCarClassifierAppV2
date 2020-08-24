652b5cf6d6559821b381fb8436fb1676c3e11f4c
Georgi S. Georgiev <Georgi.Georgiev.99@seznam.cz>
2020-08-24 03:35:29 +0200

    Well, well, well... The Drive sharing already works and it is usable. Project is saved... for now...

--- app/src/main/java/com/example/thecarrecognizer/GoogleDriveController.java
+++ app/src/main/java/com/example/thecarrecognizer/GoogleDriveController.java
@@ -2,9 +2,11 @@ package com.example.thecarrecognizer;
 
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
+import com.google.api.client.googleapis.batch.BatchRequest;
 import com.google.api.client.http.FileContent;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.Permission;
 
 import java.io.IOException;
 import java.util.concurrent.Executor;
@@ -23,13 +25,19 @@ public class GoogleDriveController {
     public Task<String> CreateImageFile(java.io.File inputFile) {
         return Tasks.call(driveExecutor, () -> {
             File fileMetaData = new File();
-            fileMetaData.setName("TheImage");
+            fileMetaData.setName("TheImage.png");
 
             FileContent mediaContent = new FileContent("application/png", inputFile);
 
             File driveFile = null;
             try {
                 driveFile = driveService.files().create(fileMetaData, mediaContent).execute();
+                Permission userPermission = new Permission()
+                        .setType("user")
+                        .setRole("reader")
+                        .setEmailAddress("gogi.gig.99@gmail.com"/*"cars.bmw.99@gmail.com"*/); // this thing works!!!
+                driveService.permissions().create(driveFile.getId(), userPermission)
+                        .setFields("id").execute();
             } catch (Exception e) {
                 e.printStackTrace();
             }
