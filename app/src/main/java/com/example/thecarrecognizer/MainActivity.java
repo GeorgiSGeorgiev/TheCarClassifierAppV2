2704ad780560d7f06416cf8bec5986b4febdd2f6
Georgi S. Georgiev <Georgi.Georgiev.99@seznam.cz>
2021-05-02 02:07:39 +0200

    Added more comments.

--- app/src/main/java/com/example/thecarrecognizer/MainActivity.java
+++ app/src/main/java/com/example/thecarrecognizer/MainActivity.java
@@ -39,26 +39,28 @@ import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
 // Note 2: the main program which does the evaluation will be referred as "the core"
 
 public class MainActivity extends AppCompatActivity implements PhotoUtilityActivityInterface {
+    // result store point so the result can be showed more than once on user request
     String evaluationResult;
     SharedPreferences sharedAppPref; // the shared settings of the application
 
-    // Main buttons
+    // The main and only buttons of this activity
     Button selectPhotoButton;
     Button evalButton;
     Button showResultsButton;
 
     static ConstraintLayout mainLayout; // the layout of the class
-    // no other instances of this class are meant to exist than the default one
+    // there is only one main activity active
 
     // The image which will be displayed on the application after selection
     ImageView mainImageView;
 
+    // items from the "Select Photo" dialog
     private static final String takePhoto = "Take Photo";
     private static final String chooseFromGallery = "Choose from Gallery";
     private static final String backStr = "Back";
 
     String currentPhotoPath = "";
-    Bitmap currentPhotoBitmap;
+    Bitmap currentPhotoBitmap; // the bitmap of the photo to be shown to the user
 
     // global application settings
     public static String serverIP;
@@ -69,8 +71,11 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
     // activity listener for taking a photo from the camera
     ActivityResultLauncher<Intent> takePhotoLauncher;
 
-    // This method is called automatically on application start.
-    // It is the best place to put initialization code.
+    /**
+     * This method is called automatically on application start and serves as a main activity
+     * initialization point.
+     * @param savedInstanceState A Bundle that restores the activity to its last saved state.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //<editor-fold desc="Main application initializations">
@@ -78,20 +83,19 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         // Run the original on Create constructor from the parent class AppCompatActivity.
         // Makes the default initializations that the application needs.
         super.onCreate(savedInstanceState);
-        // Set the main view.
+        // Set the main view of the activity.
+        // Contains the different buttons, labels, etc. of the activity.
         setContentView(R.layout.activity_main);
 
-        mainLayout = findViewById(R.id.mainLayout);
+        mainLayout = findViewById(R.id.mainLayout); // needed to change the background color
 
         // Buttons init:
         selectPhotoButton = findViewById(R.id.selectPhotoButton);
-
         evalButton = findViewById(R.id.evalButton);
         showResultsButton = findViewById(R.id.showResultsButton);
         showResultsButton.setEnabled(false);
-        // End of buttons init.
 
-        // the view (panel) where the selected image is shown
+        // the view where the selected photo is shown to the user
         mainImageView = findViewById(R.id.mainImageView);
         // set the default image to be shown on application start
         this.currentPhotoBitmap = BitmapFactory.decodeResource(this.getResources(),
@@ -110,7 +114,6 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
                         Intent data = result.getData();
                         if (data != null) {
                             Uri selectedPhotoUri = data.getData();
-                            System.out.println(selectedPhotoUri);
                             decodeAndShowPhotoFromUri(selectedPhotoUri);
                             // after the image has been changed the show results button is disabled
                             showResultsButton.setEnabled(false);
@@ -126,8 +129,8 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         this.currentPhotoBitmap = ImageBuilder.decodeAndShowPhoto(
                                 currentPhotoPath, mainImageView
-                        );
-                        // after new image has been taken the show results button is disabled
+                        ); // take the photo and then show it to the user via the ImageView
+                        // after a new image has been taken the show results button is disabled
                         showResultsButton.setEnabled(false);
                     }
                 });
@@ -136,7 +139,7 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
     }
 
     /**
-     * Creates an image selection dialog.
+     * Creates an image selection dialog. Allows to take a new photo or to choose an existing one.
      * @param view The main view where the dialog will be created.
      */
     public void selectPhoto(View view) {
@@ -160,7 +163,7 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
                     break;
             }
         };
-
+        // set the dialog color according to the chosen application theme
         if (ThemeController.chosenTheme == ThemeController.LIGHT) {
             alertDiaBuilder.setItems(options, dialogOnClickL);
         } else if (ThemeController.chosenTheme == ThemeController.DARK) {
@@ -172,12 +175,17 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         alertDiaBuilder.show();
     }
 
+    /**
+     * Finds, decodes a photo from its Uri, shows it on the main image view and then
+     * saves it as a bitmap which can be processed further in other methods.
+     * @param photoURI The Uri address of the photo.
+     */
     public void decodeAndShowPhotoFromUri(Uri photoURI) {
         mainImageView.setImageURI(photoURI);
-        this.currentPhotoBitmap = ((BitmapDrawable) mainImageView
-                .getDrawable()).getBitmap();
+        this.currentPhotoBitmap = ((BitmapDrawable) mainImageView.getDrawable()).getBitmap();
     }
 
+    // get the dialog style ID directly from the resources
     private int getDialogThemeID() {
         int themeID = 0;
         if (ThemeController.chosenTheme == ThemeController.LIGHT) {
@@ -188,7 +196,7 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         return themeID;
     }
 
-    // Main entry point
+    // Main entry point of the photo taking.
     public void handleTakePhotoIntent() {
         if (getApplicationContext().getPackageManager().hasSystemFeature(
                 PackageManager.FEATURE_CAMERA)) {
@@ -207,6 +215,7 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         }
     }
 
+    // camera permission launcher
     private final ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted-> {
                 if (isGranted) {
@@ -218,6 +227,7 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
                 }
             });
 
+    // The actual method that starts the camera and saves the result to the memory
     private void takePhoto() {
         // Intent describes the action that we want to do. So create one.
         Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -225,7 +235,7 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         try {
             // try to save the photo
             resultPhotoFile = ImageBuilder.createEmptyPhotoFile(MainActivity.this);
-            currentPhotoPath = resultPhotoFile.getAbsolutePath();
+            this.currentPhotoPath = resultPhotoFile.getAbsolutePath();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -243,7 +253,11 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         }
     }
 
-
+    /**
+     * Method that handles the photo evaluation process. Asks to send the photo for remote
+     * evaluation or starts the direct evaluation via the neural network in the application.
+     * @param view The view which is connected with calls this method.
+     */
     public void startEvaluationProcess(View view) {
         String selectedMode = sharedAppPref.getString("mode",
                 getString(R.string.default_mode));
@@ -257,6 +271,10 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         }
     }
 
+    /**
+     * Asks the user whether to continue the direct image evaluation.
+     * Upon confirmation proceeds with the evaluation otherwise cancels the operation.
+     */
     public void createEvalImgConfirmDialog() {
         int themeID = getDialogThemeID();
         AlertDialog.Builder alertDiaBuilder = new AlertDialog.Builder(MainActivity.this,
@@ -268,6 +286,11 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         alertDiaBuilder.show();
     }
 
+    /**
+     * Asks the user to confirm whether to proceed with the photo sending to the server for
+     * distant evaluation. Upon confirmation the method tries to connect with the server.
+     * The connection settings can be changed via the application settings menu.
+     */
     public void createSendImgConfirmDialog() {
         AlertDialog.Builder alertDiaBuilder = new AlertDialog.Builder(MainActivity.this);
         String alertMessage = "The selected image will be sent to the server for evaluation. " +
@@ -278,6 +301,7 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         alertDiaBuilder.show();
     }
 
+    // evaluates the neural network model directly and saves the evaluation result
     private void handleModel() {
         MLModel model = new MLModel(MainActivity.this);
         ModelResultPair[] results = model.evalDirectly(this.currentPhotoBitmap);
@@ -286,12 +310,15 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         this.showResultsButton.setEnabled(evaluationResult != null && !evaluationResult.equals(""));
     }
 
+    // tries to connect with the distant server which is supposed to make the evaluation and return
+    // the final result to the application. The connection won't be successful if the wrong settings
+    // were added or of the server is offline.
     public void handleServerCommunication() {
         OkHTTP3Controller okHTTP3Controller = new OkHTTP3Controller(MainActivity.this,
                 serverIP, portNumber);
         // If the controller IP address is private that means the server and the mobile device must
         // both be connected to it. Otherwise the data transfer won't work.
-        // Port 5000 is a TCP/UDP port and is widely used.
+        // The default port 5000 is a TCP/UDP port and is often used for such communications.
         try {
             okHTTP3Controller.connectServer(this.currentPhotoBitmap);
         } catch (Exception e) {
@@ -302,12 +329,17 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
 
     /**
      * Gets the result data and writes them on the selected view in a special dialog.
-     * @param view The view where the dialog containing all the date will be shown.
+     * @param view The view which calls the method.
      */
     public void getDirectResult(View view) {
         this.showAlertDialog(this.evaluationResult, false);
     }
 
+    /**
+     * Basic alert dialog construction and showing tool.
+     * @param message The actual message to be displayed on the dialog.
+     * @param cancelable Boolean which defines whether the Alert dialog will be cancelable or not.
+     */
     public void showAlertDialog(String message, Boolean cancelable) {
         int themeID = getDialogThemeID();
         if (message != null && !message.equals("")) {
@@ -319,6 +351,13 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         }
     }
 
+    /**
+     * Called directly after the options menu has been created. Used to initialize the different
+     * menu items. From the application the menu can be activated by clicking on the tipple dots
+     * button in the upper right corner.
+     * @param menu The actual menu which has been created.
+     * @return Returns true for the menu to be displayed.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -326,6 +365,11 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         return true;
     }
 
+    /**
+     * Called after the user selects a menu item. Used to process the item selection.
+     * @param item The menu item which was selected by the user.
+     * @return True to process the item selection.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -337,7 +381,12 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
         return super.onOptionsItemSelected(item);
     }
 
-    private void updateAppSettings() {
+    /**
+     * Gets the saved settings from the application cache and restores them.
+     * Uses shared Application Preferences. If setting was corrupted then loads its default value
+     * to prevent the application from crashing.
+     */
+    public void updateAppSettings() {
         // Restore the application theme.
         String theme = sharedAppPref.getString("theme", getString(R.string.default_theme));
         ThemeController.setChosenTheme(theme);
@@ -346,13 +395,18 @@ public class MainActivity extends AppCompatActivity implements PhotoUtilityActiv
 
         serverIP = sharedAppPref.getString("server_IP", getString(R.string.default_ip));
         String port = sharedAppPref.getString("server_port", getString(R.string.default_port));
-        portNumber = Integer.parseInt(port);
+        portNumber = Integer.parseInt(port); // set the port number
     }
 
+    /**
+     * Changes the background color of the actual active MainActivity layout.
+     * @param color The new color to be set as a background.
+     */
     public static void setBackgroundColor(int color) {
         mainLayout.setBackgroundColor(color);
     }
 
+    //
     private void updateBackground() {
         if (ThemeController.chosenTheme == ThemeController.LIGHT) {
             setBackgroundColor(getColor(R.color.White));
