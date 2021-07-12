d7db148ca34bf634bf90e31201161d097b284247
Georgi S. Georgiev <Georgi.Georgiev.99@seznam.cz>
2021-07-12 23:30:25 +0200

    Comments added.

--- app/src/main/java/com/example/thecarrecognizer/ThemeController.java
+++ app/src/main/java/com/example/thecarrecognizer/ThemeController.java
@@ -6,12 +6,19 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+/**
+ * The main controller of the application themes.
+ */
 public class ThemeController {
     public static final int LIGHT = 0;
     public static final int DARK = 1;
 
     public static int chosenTheme = LIGHT;
 
+    /**
+     * Set the application theme. At this moment can be only Light or Dark.
+     * @param theme The theme name. Can be set to: "Light" or "Dark".
+     */
     public static void setChosenTheme(String theme) {
         switch (theme) {
             case "Light":
@@ -23,23 +30,30 @@ public class ThemeController {
         }
     }
 
+    /**
+     * Create a custom-colored list of items.
+     * @param context The context where the list is located.
+     * @param options The full list of options which are located in the ListView.
+     * @param textColor The new text color of the items.
+     */
     public static ArrayAdapter<String> getCustomColoredListItemsAdapter(
             Context context, String[] options, int textColor) {
         return new ArrayAdapter<String>
                 (context, android.R.layout.simple_list_item_1, options){
             @Override
             public View getView(int position, View convertView, ViewGroup parent){
-                // Get the Item from ListView
-                View parentView = super.getView(position, convertView, parent);
+                // Get the Item on the given position directly from the list.
+                // To get the item call the original implementation of this function which is
+                // inside the ArrayAdapter class.
+                View itemView = super.getView(position, convertView, parent);
+                // Get the TextView of the selected Item. "text1" defines a normal-sized text label.
+                TextView itemTextView = itemView.findViewById(android.R.id.text1);
 
-                // Initialize a TextView for each item of the list.
-                TextView textView = parentView.findViewById(android.R.id.text1);
+                // Set the color of the text inside the selected list item.
+                itemTextView.setTextColor(textColor);
 
-                // Set the text color of the list items.
-                textView.setTextColor(textColor);
-
-                // Return the modified parent view.
-                return parentView;
+                // Return the modified item view.
+                return itemView;
             }
         };
     }
