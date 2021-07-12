2181a5cb690a84ae3465e8662738cb151ca055bf
Georgi S. Georgiev <Georgi.Georgiev.99@seznam.cz>
2021-07-12 18:44:08 +0200

    Comments added.

--- app/src/main/java/com/example/thecarrecognizer/ModelResultPair.java
+++ app/src/main/java/com/example/thecarrecognizer/ModelResultPair.java
@@ -7,22 +7,57 @@ import java.util.Map;
 
 import javax.annotation.Nonnull;
 
+/**
+ * Class representing one prediction. It contains a class label and a probability.
+ */
 public class ModelResultPair {
     private String label;
     private Float probability;
 
+    /**
+     * Basic constructor.
+     * @param otherEntry Map entry which will be converted to a ModelResultPair.
+     */
     public ModelResultPair(Map.Entry<String, Float> otherEntry) {
+        super(); // call the object constructor first
+        setTheWholePair(otherEntry); // set the pair
+    }
+
+    /**
+     * Basic constructor.
+     * @param otherEntry A pair which will be converted to a ModelResultPair.
+     */
+    public ModelResultPair(Pair<String, Float> otherEntry) {
         super();
         setTheWholePair(otherEntry);
     }
 
+    /**
+     * Basic constructor.
+     * @param labelName The label name to be put in this new instance.
+     * @param inProbability The probability which belongs to the label..
+     */
+    public ModelResultPair(String labelName, Float inProbability) {
+        super();
+        this.label = labelName;
+        this.probability = inProbability;
+    }
+
+    /**
+     * Standard hashing method.
+     * @return The resulting hash code.
+     */
     public int hashCode() {
-        // standard hashing method
-        int hashLabel = label != null ? label.hashCode() : 0;
+        int hashLabel = label != null ? label.hashCode() : 0; // if !null then label.hash else 0
         int hashProb = probability != null ? probability.hashCode() : 0;
         return (hashLabel + hashProb) * hashProb + hashLabel;
     }
 
+    /**
+     * Standard equality comparison method.
+     * @param other The other object we are comparing this one to.
+     * @return True or false.
+     */
     public boolean equals(Object other) {
         boolean compRes = false;
         if (other instanceof ModelResultPair) {
@@ -36,33 +71,62 @@ public class ModelResultPair {
         return compRes;
     }
 
+    /**
+     * Convert this instance to string. It has the following format:
+     * "label, (probability*100)%", where the probability is rounded up to two decimal places.
+     * @return The result string.
+     */
     @Nonnull
     public String toString() {
         DecimalFormat df = new DecimalFormat("0.00");
-        return label + ":  " + df.format(probability * 100) + "%";
+        return label + ", " + df.format(probability * 100) + "%";
     }
 
+    /**
+     * Get the label of this instance.
+     * @return The class name.
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * Set the label of this instance.
+     * @param label The new label which will be set.
+     */
     public void setLabel(String label) {
         this.label = label;
     }
 
+    /**
+     * Get the probability of this instance.
+     * @return The probability value asa float.
+     */
     public Float getProbability() {
         return probability;
     }
 
+    /**
+     * Set the probability of this instance.
+     * @param probability The new probability which will be set as a float.
+     */
     public void setProbability(Float probability) {
         this.probability = probability;
     }
 
+    /**
+     * Set the whole instance of this class to a new value.
+     * @param otherPair The new pair which will update this instance.
+     */
     public void setTheWholePair(Pair<String, Float> otherPair) {
         this.label = otherPair.first;
         this.probability = otherPair.second;
     }
 
+    /**
+     * Set the whole instance of this class to a new value.
+     * @param otherEntry The new map entry which will update this instance.
+     */
     public void setTheWholePair(Map.Entry<String, Float> otherEntry) {
         this.label = otherEntry.getKey();
         this.probability = otherEntry.getValue();
